4d29f0651104fad6b65c1a43934e96746f36a2a0
br-ad <brad@constellationco.com>
2010-08-21 22:20:07 +0000

    git-svn-id: https://haven.kmcnally.net/repos/constellation-myrtlealley@325 81754248-ed36-4744-880e-386003e8af05

--- trunk/searchform.php
+++ trunk/searchform.php
@@ -1,6 +1,6 @@
 <form action="<?php bloginfo('url'); ?>" method="get">
 	<fieldset class="search">
 		<input type="text" name="s" id="input" value="<?php the_search_query();?>" />
-		<input type="submit" id="searchsubmit" value="Go"/>
+		<input type="submit" id="searchsubmit" value=""/>
 	</fieldset>
 </form>
