53d81e6e076e113ef220555858a509391adbf23d
Sparticuz <kyle@kmcnally.net>
2010-08-21 21:45:19 +0000

    removed some 2010 added missing files
    git-svn-id: https://haven.kmcnally.net/repos/constellation-myrtlealley@319 81754248-ed36-4744-880e-386003e8af05

--- trunk/comments.php
+++ trunk/comments.php
@@ -7,16 +7,16 @@
  * handled by a callback to twentyten_comment which is
  * located in the functions.php file.
  *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
+ * @package Constellation
+ * @subpackage Constellation_Theme
+ * @since Constellation Theme 2.0
  */
 ?>
 
-			<div id="comments">
+	<section id="comments">
 <?php if ( post_password_required() ) : ?>
-				<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'twentyten' ); ?></p>
-			</div><!-- #comments -->
+		<div class="nopassword">This post is password protected. Enter the password to view any comments.</div>
+	<?php echo '</section><!-- #comments -->'; ?>
 <?php
 		/* Stop the rest of comments.php from being processed,
 		 * but don't kill the script entirely -- we still have
@@ -31,34 +31,29 @@
 ?>
 
 <?php if ( have_comments() ) : ?>
-			<h3 id="comments-title"><?php
-			printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), 'twentyten' ),
+			<h4 id="comments-title"><?php
+			printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number()),
 			number_format_i18n( get_comments_number() ), '<em>' . get_the_title() . '</em>' );
-			?></h3>
+			?></h4>
 
-<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+<?php if ( get_comment_pages_count() > 1 ) : // Are there comments to navigate through? ?>
 			<div class="navigation">
-				<div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'twentyten' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
+				<div class="nav-previous"><?php previous_comments_link( '<span class="meta-nav">&larr;</span> Older Comments' ); ?></div>
+				<div class="nav-next"><?php next_comments_link( 'Newer Comments <span class="meta-nav">&rarr;</span>' ); ?></div>
 			</div> <!-- .navigation -->
 <?php endif; // check for comment navigation ?>
 
 			<ol class="commentlist">
 				<?php
-					/* Loop through and list the comments. Tell wp_list_comments()
-					 * to use twentyten_comment() to format the comments.
-					 * If you want to overload this in a child theme then you can
-					 * define twentyten_comment() and that will be used instead.
-					 * See twentyten_comment() in twentyten/functions.php for more.
-					 */
-					wp_list_comments( array( 'callback' => 'twentyten_comment' ) );
+					/* Loop through and list the comments. */
+					wp_list_comments('type=comment&callback=constellation_comment');
 				?>
 			</ol>
 
-<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+<?php if ( get_comment_pages_count() > 1 ) : // Are there comments to navigate through? ?>
 			<div class="navigation">
-				<div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'twentyten' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
+				<div class="nav-previous"><?php previous_comments_link( '<span class="meta-nav">&larr;</span> Older Comments' ); ?></div>
+				<div class="nav-next"><?php next_comments_link( 'Newer Comments <span class="meta-nav">&rarr;</span>' ); ?></div>
 			</div><!-- .navigation -->
 <?php endif; // check for comment navigation ?>
 
@@ -68,12 +63,10 @@
 	 * let's leave a little note, shall we?
 	 */
 	if ( ! comments_open() ) :
-?>
-	<p class="nocomments"><?php _e( 'Comments are closed.', 'twentyten' ); ?></p>
-<?php endif; // end ! comments_open() ?>
+	endif; // end ! comments_open() ?>
 
 <?php endif; // end have_comments() ?>
 
 <?php comment_form(); ?>
 
-</div><!-- #comments -->
+</section><!-- #comments -->
---
+++ trunk/taxonomy.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying Taxonomy Pages.
+ *
+ * @package Constellation
+ * @subpackage Constellation_Theme
+ * @since Constellation Theme 2.0
+ */
+?>
+
+<?php get_header(); ?>
+<div class="grid_16">
+	<h5><?php $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) ); echo $term->name; ?></h5>
+	<div class="grid_12" style="margin-left:-1px;"><h4><?php echo term_description( '', get_query_var( 'taxonomy' ) ); ?></h4></div>
+</div>
+<?php while( have_posts() ) : the_post() ?>
+
+<div id="post-<?php the_ID(); ?>" class="grid_4 project" style="background:url('<?php 
+			echo get_post_meta (get_the_ID(), "thumb", true); ?>'); margin-bottom: 20px;">
+	<a href="<?php the_permalink(); ?>" rel="bookmark" style="display:block; height:210px;padding: 10px 0 0 10px;">
+            <span style="margin: 0;"><?php the_title() ?></span>
+	</a>
+</div><!-- .post -->
+<?php endwhile; ?>
+<?php get_footer(); ?>
