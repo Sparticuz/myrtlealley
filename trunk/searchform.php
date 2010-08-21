d0beb69dd113e177cf64a95e81cca901e83e8547
br-ad <brad@constellationco.com>
2010-08-21 22:34:26 +0000

    git-svn-id: https://haven.kmcnally.net/repos/constellation-myrtlealley@330 81754248-ed36-4744-880e-386003e8af05

--- trunk/searchform.php
+++ trunk/searchform.php
@@ -2,7 +2,7 @@
 	<fieldset class="search">
 		<input type="text" name="s" id="input" value="<?php the_search_query();?>" />
 		<input type="submit" id="searchsubmit" value=""/>
-		<img src="images/scrap/searchdecal.png" />
 	</fieldset>
+	<img src="images/scrap/searchdecal.png" />
 	
 </form>
