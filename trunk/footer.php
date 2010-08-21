e732d915b3c103e4cee566533843896f2bb8f6ff
br-ad <brad@constellationco.com>
2010-08-21 23:05:00 +0000

    git-svn-id: https://haven.kmcnally.net/repos/constellation-myrtlealley@335 81754248-ed36-4744-880e-386003e8af05

--- trunk/footer.php
+++ trunk/footer.php
@@ -11,7 +11,7 @@
  */
 ?>
 	</div><!-- #main -->
-<img src="images/scrap/maindecal.png" />
+	<?php bloginfo('template_directory'); ?>images/scrap/maindecal.png
 	<div id="footer" role="contentinfo">
 		<div id="colophon">
 
