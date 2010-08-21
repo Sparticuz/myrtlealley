7f4f64a5babbebae36fbf10a6d74cf65c9afa9e5
Sparticuz <kyle@kmcnally.net>
2010-08-21 23:30:06 +0000

    started working on the menu
    git-svn-id: https://haven.kmcnally.net/repos/constellation-myrtlealley@340 81754248-ed36-4744-880e-386003e8af05

--- trunk/header.php
+++ trunk/header.php
@@ -4,38 +4,18 @@
  *
  * Displays all of the <head> section and everything up till <div id="main">
  *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
+ * @package Constellation
+ * @subpackage Myrtle_Alley
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<title><?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 */
-	global $page, $paged;
-
-	wp_title( '|', true, 'right' );
-
-	// Add the blog name.
-	bloginfo( 'name' );
-
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
-
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
-
-	?></title>
+<title><?php bloginfo( 'name' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link rel="alternate" type="application/rss+xml" title="Constellation &amp; Co. News Feed" href="<?php bloginfo('rss2_url'); ?>" />
 <?php
 	/* We add some JavaScript to pages with the comment form
 	 * to support sites with threaded comments (when in use).
@@ -55,36 +35,22 @@
 <body <?php body_class(); ?>>
 <div id="wrapper" class="hfeed">
 	<div id="header">
-		<div id="masthead">
-			<div id="branding" role="banner">
-				<?php $heading_tag = ( is_home() || is_front_page() ) ? 'h1' : 'div'; ?>
-				<<?php echo $heading_tag; ?> id="site-title">
-					<span>
-						<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-					</span>
-				</<?php echo $heading_tag; ?>>
-				<div id="site-description"><?php bloginfo( 'description' ); ?></div>
-
-				<?php
-					// Check if this is a post or page, if it has a thumbnail, and if it's a big one
-					if ( is_singular() &&
-							has_post_thumbnail( $post->ID ) &&
-							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail' ) ) &&
-							$image[1] >= HEADER_IMAGE_WIDTH ) :
-						// Houston, we have a new header image!
-						echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
-					else : ?>
-						<img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
-					<?php endif; ?>
-			</div><!-- #branding -->
-
-			<div id="access" role="navigation">
-			  <?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff */ ?>
-				<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentyten' ); ?>"><?php _e( 'Skip to content', 'twentyten' ); ?></a></div>
-				<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */ ?>
-				<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
-			</div><!-- #access -->
-		</div><!-- #masthead -->
+		<div class="logo"><img src="<?php bloginfo('template_directory'); ?>/images/logo.png" alt="Myrtle Alley Press" /></div>
+		<div id="search"><?php get_search_form(); ?></div>
+		<div id="menu">
+			<?php 
+			/* Our navigation menu.
+			 * If one isn't filled out, wp_nav_menu falls back to wp_page_menu.
+			 * The menu assiged to the primary position is the one used.  
+			 * If none is assigned, the menu with the lowest ID is used.  
+			 */ 
+			
+			$args = array(
+				'depth' => 1
+			);
+			
+			wp_nav_menu( $args ); ?>
+		</div>
 	</div><!-- #header -->
 
-	<div id="main">
+<div id="main">
--- trunk/searchform.php
+++ trunk/searchform.php
@@ -2,7 +2,8 @@
 	<fieldset class="search">
 		<input type="text" name="s" id="input" value="<?php the_search_query();?>" />
 		<input type="submit" id="searchsubmit" value=""/>
-	</fieldset>
+	</fieldset><img src="<?php bloginfo('template_directory'); ?>/images/scrap/searchdecal.png" />
+	
 	<img src="images/scrap/searchdecal.png" />
 	
 </form>
