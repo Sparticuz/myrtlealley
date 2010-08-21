e71e5029416f37786e154c35365db4bbae67718a
br-ad <brad@constellationco.com>
2010-08-21 23:33:09 +0000

    removed wierd stuff
    git-svn-id: https://haven.kmcnally.net/repos/constellation-myrtlealley@341 81754248-ed36-4744-880e-386003e8af05

--- trunk/searchform.php
+++ trunk/searchform.php
@@ -2,8 +2,5 @@
 	<fieldset class="search">
 		<input type="text" name="s" id="input" value="<?php the_search_query();?>" />
 		<input type="submit" id="searchsubmit" value=""/>
-	</fieldset><img src="<?php bloginfo('template_directory'); ?>/images/scrap/searchdecal.png" />
-	
-	<img src="images/scrap/searchdecal.png" />
-	
-</form>
+	</fieldset>
+	</form>
