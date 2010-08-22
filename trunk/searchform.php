68cbeebac07cd1c23fc26c39f029c1cc90b1c086
br-ad <brad@constellationco.com>
2010-08-22 19:42:58 +0000

    html5 forms support placeholder text, added that in
    git-svn-id: https://haven.kmcnally.net/repos/constellation-myrtlealley@352 81754248-ed36-4744-880e-386003e8af05

--- trunk/searchform.php
+++ trunk/searchform.php
@@ -1,6 +1,6 @@
 <form action="<?php bloginfo('url'); ?>" method="get">
 	<fieldset class="search">
-		<input type="text" name="s" id="input" value="<?php the_search_query();?>" />
+		<input type="text" name="s" id="input" value="<?php the_search_query();?>" placeholder="Search" />
 		<input type="submit" id="searchsubmit" value=""/>
 	</fieldset>
 	</form>
