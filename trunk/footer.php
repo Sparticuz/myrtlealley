9f7a41d69e6e82073696c345f99f7d5f5e15cd7b
Sparticuz <kyle@kmcnally.net>
2010-08-23 16:22:35 +0000

    moved maindecal.png
    git-svn-id: https://haven.kmcnally.net/repos/constellation-myrtlealley@365 81754248-ed36-4744-880e-386003e8af05

--- trunk/footer.php
+++ trunk/footer.php
@@ -11,25 +11,12 @@
  */
 ?>
 	</div><!-- #main -->
-	<img src="<?php bloginfo('template_directory'); ?>/images/scrap/maindecal.png" />
+	<img src="<?php bloginfo('template_directory'); ?>/images/maindecal.png" />
 	<div id="footer" role="contentinfo">
-
-
-<?php
-	/* A sidebar in the footer? Yep. You can can customize
-	 * your footer with four columns of widgets.
-	 */
-	get_sidebar( 'footer' );
-?>
-
-			<div id="site-info">
-				Copyright &copy; Myrtle Alley Press 2010 | Site by <a href="http://www.constellationco.com">Constellation & Co.</a>
-			</div><!-- #site-info -->
-
-			<!-- #site-generator -->
-
+		<div id="site-info">
+			Copyright &copy; Myrtle Alley Press 2010 | Site by <a href="http://www.constellationco.com">Constellation & Co.</a>
+		</div><!-- #site-info -->
 	</div><!-- #footer -->
-
 </div><!-- #wrapper -->
 
 <?php
@@ -37,7 +24,6 @@
 	 * tag of your theme, or you will break many plugins, which
 	 * generally use this hook to reference JavaScript files.
 	 */
-
 	wp_footer();
 ?>
 </body>
