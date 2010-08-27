5eaffb6895d7a392b47ef8f2b81ec1424825644e
br-ad <brad@constellationco.com>
2010-08-27 04:32:55 +0000

    new front page, footer is broken
    git-svn-id: https://haven.kmcnally.net/repos/constellation-myrtlealley@400 81754248-ed36-4744-880e-386003e8af05

--- front-page.php
+++ front-page.php
@@ -13,9 +13,19 @@
  */
 
 get_header(); ?>
-
-<h1>Front page</h1>
-Image goes here...
+	<div id="content" role="main" style="width:920px;">
+			<div class="entry-content">
+				
+				<div>At Myrtle Alley Press, you&#8217;ll likely find someone working with dirty hands.</p> 
+<p>We are a print shop located in West Seattle that provides artisan and fine letterpress printing. We are also a design studio that specializes in branding and graphic design. Melding modern and antiquated means of design is our specialty. We&#8217;ve found that a shiny new silver MacBook and a 1945 cast iron Vandercook printing press are the best things to come together since peanut butter and jelly.</p> 
+<p>The only time our working hands are clean are when we are pulling beautifully pressed sheets of paper from the grippers of our inky, well oiled, treasured presses. Oh, and when we&#8217;re eating sandwiches, of course.</p> 
+<p>We invite you to take a tour of the site and look at the work we&#8217;ve created, printed, and/or designed. If our work inspires any ideas you have, we&#8217;d love to hear from you.</p> 
+</div> 
+</div>
+						</div><!-- .entry-content -->
+		</div><!-- #post-## -->
+	</div><!-- #content -->
+ 
 
 
 <?php get_footer(); ?>
